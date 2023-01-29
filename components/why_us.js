6f5c95890a5421f6d466b54282637b5e99fedc5e
scopionjs <josephsiame233@gmail.com>
2023-01-29 09:21:55 +0000

    push

--- components/why_us.js
+++ components/why_us.js
@@ -3,7 +3,7 @@ const Why_us=()=>{
         <div className="why-us-wrapper">
         <span className="why-us-header-text">Why Choosing Us</span>
         <p className="why-us-main-text">
-          At Company X, we understand the importance of making a lasting impression on your guests. We strive to provide the best possible experience for you and your guests, with state-of-the-art facilities and unparalleled customer service. Our team of experts will work closely with you to ensure that every detail is taken care of, from the moment you book your event to the moment your guests leave. With a wide range of halls to choose from and customizable options to suit your needs, we're confident that we can make your event a truly memorable one. Choose Company X for your next event, and let us help you make it one to remember.
+          At Company X, we understand the importance of making a lasting impression on your guests. We strive to provide the best possible experience for you and your guests, with state-of-the-art facilities and unparalleled customer service. Our team of experts will work closely with you to ensure that every detail is taken care of, from the moment you book your event to the moment your guests leave. With a wide range of halls to choose from and customizable options to suit your needs, we are confident that we can make your event a truly memorable one. Choose Company X for your next event, and let us help you make it one to remember.
         </p>
       </div>
     )
